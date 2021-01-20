f09edef709c56dbe200807dfca775fd2a47a268e
Carlos Delgado <c.delgado@globant.com>
2021-01-19 22:03:24 -0300

    change package.json, add more commands

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -5,7 +5,7 @@ module.exports = {
     name: process.env.DATABASE_NAME || 'telegram-bot',
     user: process.env.DATABASE_USERNAME || 'test-bot',
     password: process.env.DATABASE_PASSWORD || 'test-bot',
-    port: parseInt(process.env.DATABASE_PORT, 10) || 27017,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 27018,
     host: process.env.DATABASE_HOST || 'localhost',
   },
   env: process.env.NODE_ENV || 'development',
